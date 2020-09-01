46387c38c7381131688d2ce4f753615d1df960c5
samsuccess <samsuccess36@gmail.com>
2020-09-01 21:09:41 +0300

    *homework4

--- src/main/java/homework4/GameXO.java
+++ src/main/java/homework4/GameXO.java
@@ -4,8 +4,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GameXO {
-    public static final int SIZE = 3;
-    public static int DOTS_TO_WIN = 3;
+    public static final int SIZE = 5;
+    public static int DOTS_TO_WIN = 4;
     public static final char DOT_EMPTY = '•';
     public static final char DOT_X = 'X';
     public static final char DOT_O = 'O';
@@ -40,28 +40,41 @@ public class GameXO {
         }
         System.out.println("Игра закончена");
     }
+    public static boolean winLine(int x, int y, int dx, int dy, char symb) {
+        for (int i = 0; i < DOTS_TO_WIN; i++) {
+            x = x + i * dx;
+            y = y + i * dy;
+            if (isCellValid(x, y) && map[x][y] != symb){return false;}
+        }
+        return true;
+    }
 
     public static boolean checkWin(char symb) {
-        int j = 0;
-        for (int i = 0; i < DOTS_TO_WIN; i++) {
-             if (map[i][j] == symb && map[i][j + 1] == symb && map[i][j + 2] == symb) {return true;}
-             if (map[j][i] == symb && map[j + 1][i] == symb && map[j + 2][i] == symb) {return true;}
-
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if(map[i][j] == symb){
+                    for (int k = 0; k < SIZE; k++) {
+                        // проверяем строки
+                        if (winLine(k, 0, 0, 1, symb)) {return true;}
+                        // проверяем столбцы
+                        if (winLine(0, k, 1, 0, symb)) {return true;}
+                    }
+                    // проверяем диагонали
+                    if (winLine(0, 0, 1, 1, symb)) {return true;}
+                    if (winLine(0, 0, 1, -1, symb)) {return true;}
+                }
+            }
         }
-//        if (map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) {return true;}
-//        if (map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) {return true;}
-//        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) {return true;}
-//        for (int i = 0; i < DOTS_TO_WIN * 2; i++) {
-//
-//        }
-//        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb) {return true;}
-//        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb) {return true;}
-//        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) {return true;}
-
-        if(map[0][0]==symb && map[1][1]==symb && map[2][2]==symb) {return true;}
-        if(map[2][0]==symb && map[1][1]==symb && map[0][2]==symb) {return true;}
-
         return false;
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
     }
 
     public static boolean isMapFull() {
@@ -85,6 +98,7 @@ public class GameXO {
         } while (!isCellValid(x, y));
         System.out.println("Компьютер cходил в точку " + (x + 1) + " " + (y + 1));
         map[y][x] = DOT_O;
+
     }
 
     public static void humanTurn() {
@@ -94,25 +108,18 @@ public class GameXO {
             System.out.println("Введите координаты в формате X Y");
             x = sc.nextInt() - 1;
             y = sc.nextInt() - 1;
-        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        } while (!isCellValid(x, y));
         map[y][x] = DOT_X;
     }
 
     public static boolean isCellValid(int x, int y) {
-        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
+        if (x < 0 || x > SIZE || y < 0 || y > SIZE) {
             return false;
         }
         return map[y][x] == DOT_EMPTY;
     }
 
-    public static void initMap() {
-        map = new char[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                map[i][j] = DOT_EMPTY;
-            }
-        }
-    }
+
 
     public static void printMap() {
         System.out.print("  ");
