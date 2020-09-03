55e17332d5359b6bb568a3d60290d2561c6e9c95
samsuccess <samsuccess36@gmail.com>
2020-09-03 22:26:02 +0300

    homework5

--- src/main/java/homework5/Employee.java
+++ src/main/java/homework5/Employee.java
@@ -1,5 +1,31 @@
 package homework5;
 
 public class Employee {
+    private String surname;
+    private String name;
+    private String patronymic;
+    private String post;
+    private String imail;
+    private String telefon;
+    private int wages;
+    private int age;
 
+    public Employee(String surname, String name, String patronymic, String post, String imail, String telefon, int wages, int age) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.post = post;
+        this.imail = imail;
+        this.telefon = telefon;
+        this.wages = wages;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void info() {
+        System.out.printf(" %s, %s, %s, %s, %s, %s, %d, %d\n", surname, name, patronymic, post, imail, telefon, wages, age);
+    }
 }
--- src/main/java/homework5/Office.java
+++ src/main/java/homework5/Office.java
@@ -3,5 +3,19 @@ package homework5;
 public class Office {
     public static void main(String[] args) {
 
+        Employee[] person = new Employee[5];
+
+        person[0] = new Employee("Иванов","Игорь","Сергеевич", "журналист", "ivanov@yandex.ru", "89201454898", 45000, 35);
+        person[1] = new Employee("Ивлев","Иван","Степанович", "повар", "ivlev@yandex.ru", "89229842154", 40000, 45);
+        person[2] = new Employee("Сергеев","Артём","Леонидович", "курьер", "sergeev@yandex.ru", "89804581898", 25000, 25);
+        person[3] = new Employee("Толчин","Павел","Игоревич", "водитель", "tolchin@yandex.ru", "89121454844", 35000, 43);
+        person[4] = new Employee("Тюрин","Евгений","Георгиевич", "программист", "turin@yandex.ru", "89301503448", 85000, 39);
+
+        for (int i = 0; i < person.length; i++) {
+            if (person[i].getAge() > 40) {
+                person[i].info();
+            }
+        }
     }
+
 }
